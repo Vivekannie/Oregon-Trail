e1cde52a3c4760b589ac5a31254b45e7d86e9ee6
Vivek <vivekannie@yahoo.com>
2020-07-13 22:32:54 -0400

    final commit

--- script.js
+++ script.js
@@ -18,34 +18,3 @@ class Traveler{
     return this.food + " Henrietta."
  }
 }
-class Wagon {
-    constructor(capacity){
-        this.capacity = capacity
-        this.passengers = []
-    }
-    getAvailableSeatCount(){
-        return this.capacity - this.passengers.length
-    }
-
-  join(traveler){
-     if(this.getAvailableSeatCount() > 0) {
-        this.passengers.push(traveler)
-    }
-}
-  shouldQuarantine(){
-    if ( this.passengers.some(traveler => traveler.ishealthy === false)){
-      return true
-  }  else {
-      return false
-  }
- } 
- 
- totalFood (){
-     let food = 0
-     this.passengers.forEach(traveler => this.totalFood += traveler.food)
-     return food
-    } 
-        
-}
-
-
